b7a65172164e100d826b3ff93a15ee31c0d0dd27
Maina <fisiomainalex@gmail.com>
2020-12-12 14:31:22 -0300

    mudancas na expansao dos cards de consulta

--- script/app_7.js
+++ script/app_7.js
@@ -1,19 +1,28 @@
 let botoesConsulta = document.querySelectorAll(".botoes-consulta")
 let consulta = document.querySelectorAll(".consulta")
 let textoExpansao = document.querySelectorAll(".texto-expansao")
+let mais = document.querySelectorAll(".mais")
+let menos = document.querySelectorAll(".menos")
 
-for (let i=0; i<consulta.length; i++){
-    consulta[i].onclick = function(){
-        textoExpansao[i].style.display="flex";
-        textoExpansao[i].style.paddingRight="1%";
-        consulta[i].style.height="15vh";
-        consulta[i].onclick = function(){
-            textoExpansao[i].style.display="none"
-            consulta[i].style.height="5vh";
-            botoesConsulta[i].style.display="none";
-        }     
+for (let i=0; i<mais.length; i++){
+    mais[i].onclick = function(){
+        mais[i].style.display="none";
+        menos[i].style.display="block";
+        textoExpansao[i].style.display="block";
         botoesConsulta[i].style.display="flex";
         botoesConsulta[i].style.alignItems="center";
         botoesConsulta[i].style.justifyContent="end";
+        
     }
 }
+
+for (let i=0; i<mais.length; i++){
+    menos[i].onclick = function(){
+        mais[i].style.display="block";
+        menos[i].style.display="none";
+        textoExpansao[i].style.display="none";
+        botoesConsulta[i].style.display="none";
+       
+        
+    }
+}     
